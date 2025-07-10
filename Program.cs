61029ae85da6ef8491a953444506a4c4a9984f8d
Beck <Beck@GARDEN>
2025-07-10 19:08:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Write a class: “Student” with private data members: StudentId ( int), StudentFname (string), StudentLname (string ),
+// StudentGrade ( char ) and public properties for each data member. Instantiate the class and assign data to properties.
+// Display the data back on console.
+
+namespace Assignment_1._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            Student student1 = new();
+
+            // Assign data to properties
+            student1.StudentID = 1;
+            student1.StudentFirstName = "Bobby";
+            student1.StudentLastName = "Boucher";
+            student1.StudentGrade = 'D';
+
+            // Add a second student
+            Student student2 = new();
+            student2.StudentID = 2;
+            student2.StudentFirstName = "Peter";
+            student2.StudentLastName = "Parker";
+            student2.StudentGrade = 'A';
+
+            // Display student information
+            student1.DisplayStudentInfo(student1);
+            student2.DisplayStudentInfo(student2);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,47 @@
+﻿// Write a class: “Student” with private data members: StudentId ( int), StudentFname (string), StudentLname (string ),
+// StudentGrade ( char ) and public properties for each data member. Instantiate the class and assign data to properties.
+// Display the data back on console.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1._4._2
+{
+    public class Student
+    {
+        //create a class called Student
+        // with private fields (e.g., private string _studentFirstName)
+        //create public properties for each field
+        // StudentID (int), StudentFirstName (str), StudentLastName (str), StudentGrade (char)
+
+        private int _studentID;
+        public int StudentID
+        {
+            get
+            {
+                return _studentID;
+            }
+
+            set
+            {
+                _studentID = value;
+            }
+        }
+
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public char StudentGrade { get; set; }
+
+        public void DisplayStudentInfo(Student student)
+        {
+            Console.WriteLine("Student ID: ".PadLeft(12) + student.StudentID);
+            Console.WriteLine("First name: ".PadLeft(12) + student.StudentFirstName);
+            Console.WriteLine("Last name: ".PadLeft(12) + student.StudentLastName);
+            Console.WriteLine("Grade: ".PadLeft(12) + student.StudentGrade);
+            Console.WriteLine(); //extra line to separate multiple students
+        }
+    }
+}
